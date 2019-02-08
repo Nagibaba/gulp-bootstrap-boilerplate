bed869c7f897cf813e912fe59cfa71d376c567f2
nagibaba <bnagiyev@gmail.com>
2019-02-08 12:12:23 +0400

    deleted libs folder and edited notifier

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,9 @@ var autoprefixer = require('autoprefixer-core');
 var rigger = require('gulp-rigger');
 var minify = require('gulp-minify');
 var minifyCSS=require('gulp-minify-css');
-var rename = require("gulp-rename"); //minify elediyimiz dosyanın adını değiştirmek için kullandık bunu
-var concat = require('gulp-concat'); //tüm js dosyalarını birleştirip all.js diye bir js dosyasında toplamak için
-var flatten = require('gulp-flatten'); //fondaki klasör altında olan fontları tek klasöre toplamak için
-// var livereload = require('gulp-livereload');
+var rename = require("gulp-rename"); 
+var concat = require('gulp-concat'); 
+var flatten = require('gulp-flatten'); 
 var connect = require('gulp-connect');
 var notify = require("gulp-notify");
 const imagemin = require('gulp-imagemin');
@@ -26,36 +25,32 @@ const babel = require('gulp-babel');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
-// var gutil = require('gulp-util');
 
 
 var path = {
-    build: { //Burada işlemden sonra bitmiş dosyaların nereye koyulacağını gösteriyoruz
+    build: { 
         html: 'build/',
         js: 'build/js/',
-        vendor: 'build/js/vendor/', //src deki vendor klasörünü buildeki vendor klasörüne eklemek için 
+        vendor: 'build/js/vendor/', 
         css: 'build/css/main/',
         images: 'build/img/',
         fonts: 'build/fonts/',
-        libs: 'build/libs/' //bower ile src klasörüne yüklediğim dosyaları build klasörüne eklemek için 
     },
-    src: { //Burası kaynaklar
-        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+    src: {
+        html: 'src/*.html', 
         js: 'src/js/main/**/*.js',
         vendor: 'src/js/vendor/*.js',
         css: 'src/css/main.sass',
         images: 'src/img/**/*.*',
         fonts: 'src/fonts/**/*.*',
-        libs: 'src/libs/**/'
     },
-    watch: { //Burada izlemek istediğimiz dosyaları belirtiyoruz
+    watch: { 
         html: 'src/**/*.html',
         js: 'src/js/main/*.js',
         vendor: 'src/js/vendor/*.js',
         css: 'src/css/**/*.sass',
         images: 'src/img/**/*.*',
         fonts: 'src/fonts/**/*.*',
-        libs: 'src/libs/'
     },
 };
 
@@ -78,18 +73,11 @@ gulp.task('html:build', function () {
         console.log(err)
         this.emit('end')
     })
-    .pipe(rigger()) //rigger ile dosyaları birleşdiriyoruz header footer gibi komponentleri import ediyoruz bir nevi
-    .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+    .pipe(rigger()) 
+    .pipe(gulp.dest(path.build.html)) 
     .pipe(reload())
     .pipe(
-        notify(function(){
-            if(!gulpJustStarted) {
-                gulpJustStarted = false;
-                // lastCompile = new Date()
-                 return 'HTML COMPILED'
-            }
-            return false
-        })
+        notify({ message: "HTML compiled", onLast: true })
     );
 });
 
@@ -101,13 +89,13 @@ gulp.task('js:build', function () {
         debug: true
     })
     .transform(babelify)
-    // .on('error',gutil.log)
+    
     .transform("babelify", {
         presets: ["es2015"], 
         plugins: ["add-module-exports","transform-es2015-modules-umd"]}
     ) 
     .bundle()
-    // .on('error',gutil.log)
+    
     .pipe(source('all.js'))
     .pipe(gulp.dest(path.build.js))
     // .pipe(minify({
@@ -115,38 +103,21 @@ gulp.task('js:build', function () {
 
     //         min:'.min.js'
     //     },
-    //     noSource:true //bu build altındaki js klasörüne düşen index.js yi düşürmüyor sadece min olan düşüyor 
+    //     noSource:true 
     // }))
     // .pipe(gulp.dest(path.build.js))
 
     .pipe(reload())
-    .pipe(notify("JS compiled"));
+    .pipe(notify({ message: "JS compiled", onLast: true }));
 
-    // gulp.src(path.src.js) //burdakileri al derle
-    // .pipe(babel({
-    //     presets: ['env']
-    // }))
-    // .pipe(concat('all.js'))
-    // .pipe(gulp.dest(path.build.js)) //minify etmeden all.js dosyasını ekledik aşağıdaki noSource: true  komutunu silersek buna gerek olmayacak sanırım denemedim ama mantık olarak o kod minify olunmamışını eklemesini engelliyor
-    // .pipe(minify({
-    //     ext:{
-
-    //         min:'.min.js'
-    //     },
-    //     noSource:true //bu build altındaki js klasörüne düşen index.js yi düşürmüyor sadece min olan düşüyor 
-    // }))
     
-    
-    // .pipe(gulp.dest(path.build.js)) //derlenmiş dosyayı buraya at
-    // .pipe(reload())
-    // .pipe(notify("JS compiled"));
     });
 
 
 
 gulp.task('css:build', function () {
-    gulp.src(path.src.css) //Bizim main.sass seçer
-        .pipe(sass()) //sass da derler
+    gulp.src(path.src.css) 
+        .pipe(sass()) 
         .on('error', function(err) {
             console.log(err)
             this.emit('end')
@@ -155,15 +126,15 @@ gulp.task('css:build', function () {
         .pipe(gulp.dest(path.build.css))
         .pipe(minifyCSS())
         .pipe(rename({suffix:'.min'}))
-        .pipe(gulp.dest(path.build.css)) //build deki css e atar
+        .pipe(gulp.dest(path.build.css)) 
         .pipe(reload())
-        .pipe(notify("CSS compiled"));
+        .pipe(notify({ message: "CSS compiled", onLast: true }));
     });
 
 
 
 gulp.task('image:build', function () {
-    gulp.src(path.src.images) //resimleri seçer
+    gulp.src(path.src.images) 
     .on('error', function(err) {
         console.log(err)
         this.emit('end')
@@ -171,19 +142,10 @@ gulp.task('image:build', function () {
     .pipe(imagemin())
     .pipe(gulp.dest(path.build.images))
     .pipe(reload())
-    // .pipe(notify("IMAGE compiled"));
+    
 });
 
-gulp.task('libs:build', function () {
-    gulp.src(path.src.libs) 
-    .on('error', function(err) {
-        console.log(err)
-        this.emit('end')
-    })
-    .pipe(gulp.dest(path.build.libs))
-    .pipe(reload())
-    // .pipe(notify("LIBS compiled"));
-});
+
 
 
 gulp.task('vendor:build', function () {
@@ -218,7 +180,6 @@ gulp.task('build', [
     'css:build',
     'fonts:build',
     'image:build',
-    'libs:build',
     'vendor:build'
     ]);
 
@@ -242,9 +203,6 @@ gulp.task('watch', function(){
         gulp.start('fonts:build');
     });
 
-    watch([path.watch.libs], function(event, cb) {
-        gulp.start('libs:build');
-    });
 
     watch([path.watch.vendor], function(event, cb) {
         gulp.start('vendor:build');
