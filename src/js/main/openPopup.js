9ef1e7383a85ff2b77898e756d83a158462f79ce
nagibaba <bnagiyev@gmail.com>
2019-02-28 19:20:48 +0400

    added balance page

--- src/js/main/openPopup.js
+++ src/js/main/openPopup.js
@@ -1,13 +1,15 @@
 const openPopup = ($)=> {
 	const blurryBg = $('.blurry-background')
 	const popup = $('.b-popup')
+	const bNav = $('.b-nav') // responsive issues
 	$('.openPopup').click(e=>{
 		e.preventDefault()
 		const offset = $(e.target).offset()
 		const top = offset.top
 		const left = offset.left
-		
+		bNav.css('display', 'none')
 		blurryBg.addClass('blurry-background--active')
+		// $('body').css('background-color', 'rgba(0, 0, 0, .2)')
 		popup.addClass('b-popup--active')
 		// popup.css('display', 'flex')
 	})
@@ -16,6 +18,11 @@ const openPopup = ($)=> {
 		if(target.closest('.b-popup__inner').length<=0){
 			blurryBg.removeClass('blurry-background--active')
 			popup.removeClass('b-popup--active')
+			setTimeout(function(){
+				bNav.css('display', 'flex')
+			}, 300)
+			// $('body').css('background-color', 'white')
+
 		}
 	})
 	
